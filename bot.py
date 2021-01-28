f8dfa80efcfed83ec920125b07dea3387836af94
dawidjasiorowski <75570369+dawidjasiorowski@users.noreply.github.com>
2021-01-28 09:30:05 +0100

    Initial commit

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+import os
+import krzysiu
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('#krzysiu'):
+        await message.author.edit(nick=krzysiu.getName())
+client.run(os.getenv("TOKEN"))

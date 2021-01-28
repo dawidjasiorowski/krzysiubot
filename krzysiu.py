11d1815330ef285a3d1e0d3664aee929bc82e771
dawidjasiorowski <75570369+dawidjasiorowski@users.noreply.github.com>
2021-01-28 10:17:17 +0100

    split names and adjectives to files

--- krzysiu.py
+++ krzysiu.py
@@ -1,11 +1,18 @@
 import random
 
-adjectives = ["Czekoladowy", "Lemoniadowy", "Marcepanowy", "Kisielowy", "Malinowy", "Truskawkowy", "Jablkowy", "Pomaranczowy", "Galaretkowy", "Chalwowy", "Batonikowy", "Gruszkowy", "Cytrynowy", "Owocowy", "Melonowy", "Arbuzowy", "Wodny", "Choco", "Floppa", "Giga", "Ultra"]
-names = ["John", "Lui", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles", "Christopher", "Daniel", "Matthew", "Anthony", "Donald", "Mark", "Paul", "Steven", "Andrew", "Kenneth", "Joshua", "Kevin", "Brian", "George"]
+with open("./adjectives.txt", "r") as f:
+    s = str(f)
+    adj = s.split(" ")
+with open("./names.txt", "r") as f:
+    s = str(f)
+    names = s.split(" ")
+#adjectives = ["Czekoladowy", "Lemoniadowy", "Marcepanowy", "Kisielowy", "Malinowy", "Truskawkowy", "Jablkowy", "Pomaranczowy", "Galaretkowy", "Chalwowy", "Batonikowy", "Gruszkowy", "Cytrynowy", "Owocowy", "Melonowy", "Arbuzowy", "Wodny", "Choco", "Floppa", "Giga", "Ultra"]
+#names = ["John", "Lui", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles", "Christopher", "Daniel", "Matthew", "Anthony", "Donald", "Mark", "Paul", "Steven", "Andrew", "Kenneth", "Joshua", "Kevin", "Brian", "George"]
+
 
 def getName():
     a = random.randrange(0, len(adjectives))
     b = random.randrange(0, len(names))
-    return adjectives[a] + " " + names[b]
+    return adj[a] + " " + names[b]
 
 print(getName())
